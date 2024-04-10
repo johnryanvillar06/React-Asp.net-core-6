3cf76a13066f18f4ceb50467dde9ab03d7056cb3
johnryanvillar <johnryan.villar@irely.com>
2024-04-10 20:04:02 +0800

    First commit

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className= "content">
+      <h1>
+        App componetn
+      </h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
